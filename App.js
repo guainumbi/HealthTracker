b5534a558933a9dceb0b35dba8b515c2df1e1cf8
Jessica Grzanna <jgrzanna@me.com>
2019-06-27 15:09:41 +0200

    init: Set up new app
    - Set up expo for development environment
    - Set up basic app functionality, helper methods and components with react
    - Store data locally with AsyncStorage
    - Add redux

---
+++ App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import { View } from "react-native";
+import AddEntry from "./components/AddEntry";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import reducer from "./reducers";
+
+export default class App extends Component {
+  render() {
+    return (
+      <Provider store={createStore(reducer)}>
+        <View>
+          <AddEntry />
+        </View>
+      </Provider>
+    );
+  }
+}

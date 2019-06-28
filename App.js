0b8857c5dfca29d0299f99bb487538ddb50a142e
Jessica Grzanna <jgrzanna@me.com>
2019-06-28 17:05:00 +0200

    feat: Add History and MetricCard Components
    The History component displays the logged or not logged data for specific dates from the calendar and displays them in form of MetricCards.

--- App.js
+++ App.js
@@ -4,13 +4,15 @@ import AddEntry from "./components/AddEntry";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";
+import History from "./components/History";
 
 export default class App extends Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
-          <AddEntry />
+          <View style={{ height: 20 }} />
+          <History />
         </View>
       </Provider>
     );
